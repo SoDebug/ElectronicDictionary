85184cf6fca5e6fe047984ae08388b578e59b14d
SoDebug <1363351571@qq.com>
2022-12-26 19:11:05 +0800

    [Important]:UI改写，并使用表单数据管理器而不是使用文本框

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
 from PyQt5 import QtGui, QtWidgets, QtCore
 import sys
 
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QSizePolicy
-
 # 创建主进程
 app = QtWidgets.QApplication(sys.argv)
-
 # 创建主窗口
 Main_Window = QtWidgets.QWidget()
 Main_Window.setWindowTitle('Material Input Field')
-
 
 # 创建问候语
 hello_words = QtWidgets.QLabel('知道你在努力...')
@@ -27,29 +22,29 @@ hello_words.setPalette(palette)
 # 创建布局管理器(QVBoxLayout：垂直)
 layout = QtWidgets.QVBoxLayout()
 
-# 创建文本框
-text_field = QtWidgets.QLineEdit()
-text_field.setPlaceholderText('键入你想要查询的单词...')
-# 设置输入框的最小宽高
-text_field.setMinimumSize(100, 40)
+# 使用表单数据管理器而不是简单的文本框
+form_layout = QtWidgets.QFormLayout()
+user_submit = QtWidgets.QLineEdit()
+form_layout.addRow('', user_submit)
+user_submit.setPlaceholderText('键入你想要查询的单词...')
 
-# 设置文本框的颜色
-palette = QtGui.QPalette()
-palette.setColor(QtGui.QPalette.Base, QtGui.QColor(255, 255, 255))
-text_field.setPalette(palette)
+layout.addLayout(form_layout)
 
-# 创建用户查询的提交按钮
-user_submit = QtWidgets.QPushButton('查询')
+# 创建查询提交按钮
+submit_button = QtWidgets.QPushButton('查询')
 
-# 设置按钮的颜色
-palette = QtGui.QPalette()
-palette.setColor(QtGui.QPalette.Button, QtGui.QColor(0, 150, 136))
-user_submit.setPalette(palette)
 
-# 将文本框和按钮添加到布局管理器中
-layout.addWidget(hello_words, alignment=Qt.AlignCenter)
-layout.addWidget(text_field)
-layout.addWidget(user_submit)
+# 获取输入内容
+def get_raw():
+    submit_words = user_submit.text()
+    if submit_words == 'admin':
+        QtWidgets.QMessageBox.information(Main_Window, '提示', '你查询的单词是admin，测试通过')
+    else:
+        QtWidgets.QMessageBox.critical(Main_Window, '错误', '输入字符非测试内容，或者提取字符出错')
+
+
+submit_button.clicked.connect(get_raw)
+layout.addWidget(submit_button)
 
 # 将布局管理器设置到窗口中
 Main_Window.setLayout(layout)
