566db6a40a3b1476635e61ba311663b0b4a5f9b7
SoDebug <1363351571@qq.com>
2022-12-26 17:56:27 +0800

    修复在布局管理器下无法实现控件宽高的问题

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from PyQt5 import QtGui, QtWidgets, QtCore
 import sys
 
 from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QSizePolicy
 
 # 创建应用
 app = QtWidgets.QApplication(sys.argv)
@@ -9,11 +10,10 @@ app = QtWidgets.QApplication(sys.argv)
 # 创建窗口
 window = QtWidgets.QWidget()
 window.setWindowTitle('Material Input Field')
-# 设置窗口大小
-window.resize(400, 300)
+
 
 # 创建大标题
-title = QtWidgets.QLabel('My Window Title')
+title = QtWidgets.QLabel('知道你在努力...')
 # 设置大标题的字体
 font = QtGui.QFont()
 font.setPointSize(20)
@@ -24,12 +24,14 @@ palette = QtGui.QPalette()
 palette.setColor(QtGui.QPalette.Foreground, QtGui.QColor(0, 0, 0))
 title.setPalette(palette)
 
-# 创建布局管理器
+# 创建布局管理器(QVBoxLayout：垂直)
 layout = QtWidgets.QVBoxLayout()
 
 # 创建文本框
 text_field = QtWidgets.QLineEdit()
 text_field.setPlaceholderText('键入你想要查询的单词...')
+# 设置输入框的最小宽高
+text_field.setMinimumSize(100, 50)
 
 # 设置文本框的颜色
 palette = QtGui.QPalette()
@@ -52,6 +54,8 @@ layout.addWidget(button)
 # 将布局管理器设置到窗口中
 window.setLayout(layout)
 
+# 设置窗口大小
+window.resize(400, 300)
 # 显示窗口
 window.show()
 
