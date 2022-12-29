a4d6c8f09da44bbdeec24f9a391dfe09d6febbe5
SoDebug <1363351571@qq.com>
2022-12-29 22:22:43 +0800

    [Important]:查询的程序已经实现，等待接入主程序 ‘main.py’

--- check.py
+++ check.py
@@ -1,9 +1,21 @@
 import sqlite3
-# 开始与数据库建立连接
-# 如果数据库不存在，将会自动创建一个
-connect = sqlite3.connect('database.db')
-#
-cursor = connect.cursor()
-cursor.execute('SELECT pronunciation, pos, collocations, example FROM words WHERE word="make"')
-result = cursor.fetchone()
-pronunciation, pos, collocations, example = result
+
+
+def check(query_word):
+    # 连接数据库
+    conn = sqlite3.connect('database.db')
+    # 创建游标
+    cursor = conn.cursor()
+    # 执行查询语句
+    cursor.execute("SELECT word, pronunciation, pos, collocations, examole FROM test WHERE word=?", (query_word,))
+    # 获取查询结果
+    result = cursor.fetchone()
+    # 如果查询结果为空，则返回一个包含 5 个字符 "null" 的列表
+    if result is None:
+        data = ["null", "null", "null", "null", "null"]
+    else:
+        # 将查询结果储存至列表 data 中
+        data = list(result)
+    # 关闭数据库连接
+    conn.close()
+
