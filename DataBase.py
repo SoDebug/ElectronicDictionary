9ba4c791ad565388afceaa8bbcf418f64d27d9c6
SoDebug <1363351571@qq.com>
2022-12-30 15:07:30 +0800

    [Important]:查询结果新增一项内容：otherforms

--- DataBase.py
+++ DataBase.py
@@ -7,12 +7,12 @@ def check(query_word):
     # 创建游标
     cursor = conn.cursor()
     # 执行查询语句
-    cursor.execute("SELECT word, pronunciation, pos, collocations, examole FROM test WHERE word=?", (query_word,))
+    cursor.execute("SELECT word, pronunciation, pos, otherforms, collocations, examole FROM test WHERE word=?", (query_word,))
     # 获取查询结果
     result = cursor.fetchone()
-    # 如果查询结果为空，则返回一个包含 5 个字符 "null" 的列表
+    # 如果查询结果为空，则返回一个包含 6 个字符 "null" 的列表
     if result is None:
-        data = ["null", "null", "null", "null", "null"]
+        data = ["null", "null", "null", "null", "null", "null"]
     else:
         # 将查询结果储存至列表 data 中
         data = list(result)
